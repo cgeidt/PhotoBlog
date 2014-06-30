493033bee759a66df38a2ca925d06a7ee3236fd2
c0mpas <exit>
2014-06-30 13:52:06 +0200

    finished localization added toolbar-option to switch language between DE and EN

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :photos, only: [:index]
+  post '/:locale' => "application#set_locale"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
